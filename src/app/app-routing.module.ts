84041ec8e69b75de36754409b59ee6cf3a0c5f32
AlouiOmar97 <omar.aloui@esprit.tn>
2025-11-18 16:35:28 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
 const routes: Routes = [
-  //{ path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'suggestions', component: ListSuggestionComponent}
 ];
 
 @NgModule({
